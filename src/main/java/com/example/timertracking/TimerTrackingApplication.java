256b8855cd4188c6291fb39842f7cd07664af795
Manh Chu <chuhuumanh331997@gmail.com>
2020-08-18 19:08:58 +0700

    init project

---
+++ src/main/java/com/example/timertracking/TimerTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.timertracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimerTrackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/timertracking/TimerTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timertracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimerTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
